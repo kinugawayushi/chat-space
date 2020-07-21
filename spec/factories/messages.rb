8064a872275a3cde849a3643c860dfc8bd81a186
kinugawayushi <070707yushi@gmail.com>
2020-07-21 21:36:16 +0900

    テスト

---
+++ spec/factories/messages.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :message do
+    content          {Faker::Lorem.sentence}
+    image            {File.open("#{Rails.root}/public/images/fff.jpg")}
+    user
+    group
+  end
+end
+
+
